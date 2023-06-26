db8b530d477629a90ebcc2cbfe877b1f5917bfa2
Jennifer Orosun <orosunj@gmail.com>
2023-06-26 22:55:12 +0100

    feat: added more tests for different types of drinks tea and juice

--- src/controllers/juice_controller.test.ts
+++ src/controllers/juice_controller.test.ts
@@ -2,11 +2,23 @@ import request from "supertest";
 import { app } from "../app";
 
 test("GET /juice should return correct object", async () => {
-  const res = await request(app).get("/juice").query({ coffeeName: "orange" });
+  const res = await request(app).get("/juice").query({ juiceName: "Orange" });
 
   expect(res.statusCode).toEqual(200);
   expect(res.body).toEqual({
     drinkType: "Juice",
     name: "Orange",
+  });
+});
+
+test("GET /juice with a parameter should return correct object", async () => {
+  const res = await request(app)
+    .get("/juice")
+    .query({ juiceName: "Pineapple" });
+
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Juice",
+    name: "Pineapple",
   });
 });
--- src/controllers/tea_controller.test.ts
+++ src/controllers/tea_controller.test.ts
@@ -2,11 +2,20 @@ import request from "supertest";
 import { app } from "../app";
 
 test("GET /tea should return correct object", async () => {
-  const res = await request(app).get("/tea").query({ coffeeName: "Breakfast" });
+  const res = await request(app).get("/tea").query({ teaName: "Breakfast" });
 
   expect(res.statusCode).toEqual(200);
   expect(res.body).toEqual({
     drinkType: "Tea",
     name: "Breakfast",
+  });
+});
+test("GET /tea should return correct object", async () => {
+  const res = await request(app).get("/tea").query({ teaName: "Earl Grey" });
+
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Tea",
+    name: "Earl Grey",
   });
 });
