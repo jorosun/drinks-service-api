6bc887eae4da3ee6e7a05483c3029d96bcea84cc
Jennifer Orosun <orosunj@gmail.com>
2023-06-26 15:55:19 +0100

    feat: Implement GET request for coffeelover endpoint and test

--- src/controllers/coffeelover_request.test.ts
+++ src/controllers/coffeelover_request.test.ts
@@ -1,10 +1,10 @@
 import request from "supertest";
 import { app } from "../app";
 
-describe("Test home API endpoint request", () => {
+describe("Test /coffeelover API endpoint request", () => {
   test("GET should return correct message", async () => {
-    const res = await request(app).get("/");
+    const res = await request(app).get("/coffeelover");
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toEqual("Welcome to the Drinks API!");
+    expect(res.text).toEqual("I like coffee!");
   });
 });
